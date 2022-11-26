03b712d8b6c59d46e476acd7c260f74f1f2525a0
vishwaroopa <vishwarupa.vishu@gmail.com>
2022-11-26 18:16:26 +0530

    commit

--- config/database.js
+++ config/database.js
@@ -1,15 +1,15 @@
+// path: ./my-project/config/database.js
+
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-      },
-      options: {
-        useNullAsDefault: true,
-      },
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST", "strapi-database.cylhprmjasim.ap-south-1.rds.amazonaws.com"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "mAVIN2022"),
     },
+    useNullAsDefault: true,
   },
 });

d062b283cb936c731702be877b694a120a3770d5
vishwaroopa <vishwarupa.vishu@gmail.com>
2024-01-02 16:32:04 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -4,11 +4,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "strapi-database.cylhprmjasim.ap-south-1.rds.amazonaws.com"),
+      host: env("DATABASE_HOST", "database-1.c1a2k2wquows.ap-southeast-2.rds.amazonaws.com"),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
+      database: env("DATABASE_NAME", "Mavininfotech"),
       user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "mAVIN2022"),
+      password: env("DATABASE_PASSWORD", "Mavin2024"),
     },
     useNullAsDefault: true,
   },
